283500028457f72469cd0794143538f3464b68b8
DinoStark <15190304050@163.com>
2023-07-30 13:12:36 +0800

    Rename fields & variables in JsonMessage

--- src/main/java/stark/dataworks/basic/io/net/netty/JsonMessage.java
+++ src/main/java/stark/dataworks/basic/io/net/netty/JsonMessage.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * Message that is sent/received by the executor.
+ * Message that is sent/received by netty channels, wrapped as JSON.
  */
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,18 +15,18 @@ public class JsonMessage
     /**
      * Message body to send through network.
      */
-    private Object message;
+    private Object body;
 
     /**
      * Type of the message, used to deserialize the message to an object.
      */
     private String type;
 
-    public static JsonMessage toJsonMessage(Object message)
+    public static JsonMessage toJsonMessage(Object body)
     {
-        JsonMessage executorMessage = new JsonMessage();
-        executorMessage.setMessage(message);
-        executorMessage.setType(message.getClass().getName());
-        return executorMessage;
+        JsonMessage jsonMessage = new JsonMessage();
+        jsonMessage.setBody(body);
+        jsonMessage.setType(body.getClass().getName());
+        return jsonMessage;
     }
 }
