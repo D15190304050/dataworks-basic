4833556dda987da3716ad25d7ec51c8c11e54dce
DinoStark <15190304050@163.com>
2024-08-10 00:17:49 +0800

    getAllFields

--- src/main/java/stark/dataworks/basic/beans/FieldExtractor.java
+++ src/main/java/stark/dataworks/basic/beans/FieldExtractor.java
@@ -4,6 +4,7 @@ import stark.dataworks.basic.params.ArgumentValidator;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
@@ -19,5 +20,21 @@ public class FieldExtractor
         ArgumentValidator.requireNonNull(sourceList, "sourceList");
 
         return sourceList.stream().map(mapper).toList();
+    }
+
+    public static List<Field> getAllFields(Class<?> clazz)
+    {
+        List<Field> fields = new ArrayList<>();
+
+        while (clazz != null)
+        {
+            Field[] declaredFields = clazz.getDeclaredFields();
+
+            fields.addAll(Arrays.asList(declaredFields));
+
+            clazz = clazz.getSuperclass();
+        }
+
+        return fields;
     }
 }
